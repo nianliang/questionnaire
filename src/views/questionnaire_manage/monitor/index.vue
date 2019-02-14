da272462b3984f5b800dc72d27382d172704d6b3
guangli.li <12345678>
2019-02-14 15:48:49 +0800

    问卷监督

--- src/views/questionnaire_manage/monitor/index.vue
+++ src/views/questionnaire_manage/monitor/index.vue
@@ -1,8 +1,36 @@
 <style lang="less">
-
+.monitor{
+  .monitor-sub-border{
+    border: 1px solid #ebebeb;
+    padding: 16px;
+  }
+}
 </style>
 <template>
-  <div>
-    可视化问卷监督
+  <div class="monitor flex">
+    <div class="flex-1 monitor-sub-border">
+      此处是统计数据
+    </div>
+    <div class="flex-3 monitor-sub-border">
+      此处是地图
+    </div>
+    <div class="flex-1 monitor-sub-border">
+      此处是选中地图点之后的人员列表
+    </div>
   </div>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    mounted () {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
